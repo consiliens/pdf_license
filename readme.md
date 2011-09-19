7ebc5f818decf48c3da8eea6d63326fdcd5fba95
consiliens <consiliens@gmail.com>
2011-09-19 00:21:37 -0600

    Init.

---
+++ readme.md
@@ -0,0 +1,13 @@
+JRuby ICEpdf example
+====================
+
+Usage
+-----
+
+* ```rake spec```
+* ```rake main```
+
+License
+-------
+
+pdf_license is licensed under the same terms as IcePDF.  IcePDF is used under the [MPL 1.1](http://www.mozilla.org/MPL/MPL-1.1.html).
---
+++ spec/pdf_license_spec.rb
@@ -0,0 +1,60 @@
+=begin
+Version: MPL 1.1/GPL 2.0/LGPL 2.1
+
+"The contents of this file are subject to the Mozilla Public License Version
+1.1 (the "License"); you may not use this file except in compliance with the
+License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+the specific language governing rights and limitations under the License.
+
+The Original Code is pdf_license, released June 5th, 2010. The Initial
+Developer of the Original Code is consiliens (consiliens@gmail.com). Portions
+created by consiliens are Copyright (C) 2010 consiliens
+(consiliens@gmail.com). All Rights Reserved.
+
+Alternatively, the contents of this file may be used under the terms of the
+GNU Lesser General Public License Version 2.1 or later (the "LGPL" License),
+in which case the provisions of the LGPL License are applicable instead of
+those above. If you wish to allow use of your version of this file only under
+the terms of the LGPL License and not to allow others to use your version of
+this file under the MPL, indicate your decision by deleting the provisions
+above and replace them with the notice and other provisions required by the
+LGPL License. If you do not delete the provisions above, a recipient may use
+your version of this file under either the MPL or the LGPL License."
+=end
+require 'helper.rb'
+
+# Generate the PDF files from the ODT folder to run the tests.
+describe PdfLicense do
+
+    license = <<-'L'.split "\n"
+http://creativecommons.org/licenses/by-sa/3.0/us/
+http://creativecommons.org/licenses/by-sa/3.0/us/legalcode
+    L
+
+  pdf_folder = File.expand_path '../../pdf', __FILE__
+
+
+  # RSpec inside a block doesn't fail so store results and check after.  
+  it 'should fail on no license' do
+    results = []
+
+    pdfs = PdfLicense.new "#{pdf_folder}/fail/**/*", license
+    pdfs.check { |m| results.push m[0] }
+
+    results.each { |m| m.should == false }
+  end
+
+
+  it 'should match on a license' do
+    results = []
+
+    pdfs = PdfLicense.new "#{pdf_folder}/ok/**/*", license
+    pdfs.check { |m| results.push m[0] }
+
+    results.each { |m| m.should == true }
+  end
+
+end
